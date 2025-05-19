3c0a349abddcb1aafc98359950d04101d8ac8f67
bioinagesh <nagzz@users.noreply.github.com>
2025-05-19 16:23:11 -0400

    Update peak_overlapping.sh

--- peak_overlapping.sh
+++ peak_overlapping.sh
@@ -1,3 +1,9 @@
 #Cut&Run peaks overlap with RNAseq expression data
 
 awk -F, 'NR==FNR{patterns[$0]=1; next} FNR==1{print; next} { temp=$NF; gsub(/"/, "", temp); if (patterns[temp]) print }' /fs/ess/PAS1348/nagesh/cutandrun_hannahs/output/mapping/bam/rmdups/macs3/H82_cMYC_25_75_ids.txt H82/Significant_DE_genes_H82_vs_H1048_batch_corrected.csv > H82/H82_gene_expression_associated_with_cMYC_peaks.csv
+
+#R
+library(readr)
+library(writexl)
+data <- read_csv("H82_gene_expression_associated_with_E2F1_peaks.csv")
+write_xlsx(data, "H82_gene_expression_associated_with_cMYC_peaks.xlsx")
